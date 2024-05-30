4c447da83a2a15689ad0056c6a3fb9acac392ef1
Vidhanth13 <104818521+Vidhanth13@users.noreply.github.com>
2024-05-31 01:24:39 +0200

    Add files via upload

---
+++ Models/AfriFarmEnergyContext.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace AfriEnergy_ST10146835.Models;
+
+public partial class AfriFarmEnergyContext : DbContext
+{
+    public AfriFarmEnergyContext()
+    {
+    }
+
+    public AfriFarmEnergyContext(DbContextOptions<AfriFarmEnergyContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=labVMH8OX\\SQLEXPRESS;Initial Catalog=AfriFarmEnergy;Integrated Security=True;Trust Server Certificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.ProductId).HasName("PK__Products__B40CC6EDD5502C4C");
+
+            entity.Property(e => e.ProductId).HasColumnName("ProductID");
+            entity.Property(e => e.Category).HasMaxLength(50);
+            entity.Property(e => e.ProductName).HasMaxLength(100);
+            entity.Property(e => e.UserId).HasColumnName("UserID");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Products)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK__Products__UserID__4CA06362");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.UserId).HasName("PK__Users__1788CCACBD207538");
+
+            entity.HasIndex(e => e.Email, "UQ__Users__A9D10534A3BC0EB6").IsUnique();
+
+            entity.Property(e => e.UserId).HasColumnName("UserID");
+            entity.Property(e => e.Address)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.CreatedAt).HasColumnType("datetime");
+            entity.Property(e => e.Email)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.LastName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Phone)
+                .HasMaxLength(15)
+                .IsUnicode(false);
+            entity.Property(e => e.UserPassword)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.UserRole)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Username)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/CurrentUser.cs
@@ -0,0 +1,8 @@
+﻿namespace AfriEnergy_ST10146835.Models
+{
+    // Class for storing the currently logged-in user's information
+    public class CurrentUser
+    {
+        public static string currentUser { get; set; } // Property to hold the username of the current user
+    }
+}
